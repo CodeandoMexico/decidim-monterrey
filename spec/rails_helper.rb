5c79edcfdf6060dee0e7c9661fe3aa02460095c8
Eduardo Hernandez <eduardoghdez.io@gmail.com>
2022-02-11 14:00:03 -0600

    Agregar env.example
    Asi podemos cargar una plantilla de las envs necesarias en los test, sin
    tener que agregarlos directamente en GitHub.
    
    Tambien nos sirve para mantener una plantilla para nuevas personas que
    quieran cotribuir al proyecto.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require "dotenv"
+Dotenv.load("env.example")
+
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
