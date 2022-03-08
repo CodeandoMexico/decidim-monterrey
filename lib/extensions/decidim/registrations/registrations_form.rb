002ccc79985761bb503cb44826911d21c5841d91
Eduardo Hernandez <eduardoghdez.io@gmail.com>
2022-03-07 18:06:49 -0600

    Agregar telefono de participantes a admin...
    ... y validaciones de telefono

--- lib/extensions/decidim/registrations/registrations_form.rb
+++ lib/extensions/decidim/registrations/registrations_form.rb
@@ -5,6 +5,16 @@ module Extensions
 
       included do
         attribute :phone, String
+        validates :phone, allow_nil: true, format: /[0-9]{10}/
+        validate :mobile_phone_number_unique_in_organization
+
+        def mobile_phone_number_unique_in_organization
+          existing_user = ::Decidim::User.no_active_invitation
+            .find_by(phone: phone, organization: current_organization)
+            .present?
+
+          errors.add :phone, :taken if existing_user.present?
+        end
       end
     end
   end
