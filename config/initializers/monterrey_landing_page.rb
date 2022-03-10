49834d77a1836f7bef614c336134f1b8553d4bfd
dany_herz42 <daniel.hernandez@ironbit.com.mx>
2022-03-09 19:59:38 -0600

    Creacion del cell para landing page de monterrey

---
+++ config/initializers/monterrey_landing_page.rb
@@ -0,0 +1,5 @@
+Decidim.content_blocks.register(:homepage, :monterrey_landing_page) do |content_block|
+  content_block.cell = "decidim/content_blocks/monterrey_landing_page"
+  content_block.public_name_key = "decidim.content_blocks.monterrey_landing_page.name"
+  content_block.default!
+end
