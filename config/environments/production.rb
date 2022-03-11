0aa99d0b613ce743c757fbe077dcac21106b7887
Oscar Hernandez <2067516+oxcar@users.noreply.github.com>
2022-03-10 18:29:10 -0600

    Fix queue_adapter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter = :sideqik
+  config.active_job.queue_adapter = :sidekiq
   config.active_job.queue_name_prefix = "decidim_monterrey_production"
   config.action_mailer.perform_caching = false
 
