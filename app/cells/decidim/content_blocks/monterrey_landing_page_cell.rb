a5a645b1db223d2285de6282aefa07e917e094a6
dany-herz42 <danyherzdev@gmail.com>
2022-03-14 12:54:26 -0600

    Se agregaron las traduciones para la landing page ademas de la funcion de obtener las 4 propuestas mas recientes

--- app/cells/decidim/content_blocks/monterrey_landing_page_cell.rb
+++ app/cells/decidim/content_blocks/monterrey_landing_page_cell.rb
@@ -7,16 +7,30 @@ module Decidim
       include Decidim::SanitizeHelper
 
       def show
+        get_proposals(2)
         return if translated_attribute(current_organization.description).blank?
 
         render
+      end
+
+      def get_proposals(limit)
+        proposals = ::Decidim::Proposals::Proposal.all
+        return false if proposals.length.nil?
+        return proposals.reverse if proposals.length < limit
+
+        proposals = proposals.reverse
+        proposals_return = []
+        (limit - 1).times do |i|
+          proposals_return.push(proposals[i])
+        end
+
+        proposals_return
       end
 
       private
 
       def organization_description
         desc = decidim_sanitize(translated_attribute(current_organization.description))
-
         # Strip the surrounding paragraph tag because it is not allowed within
         # a <hN> element.
         desc.gsub(%r{</p>\s+<p>}, "<br><br>").gsub(%r{<p>(((?!</p>).)*)</p>}mi, "\\1")
