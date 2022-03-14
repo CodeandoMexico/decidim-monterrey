0fb6a0e2047159f1416b22701004ab67566fcd82
Oscar Hernandez <2067516+oxcar@users.noreply.github.com>
2022-03-14 13:24:14 -0600

    Fix RSpec

--- config/initializers/budgets_workflows.rb
+++ config/initializers/budgets_workflows.rb
@@ -1,3 +1,3 @@
 Rails.application.config.to_prepare do
-  require "./lib/decidim/budgets/workflows"
+  require "./lib/extensions/decidim/budgets/workflows"
 end
--- lib/decidim/budgets/workflows.rb
+++
@@ -1,3 +0,0 @@
-require "decidim/budgets/workflows/monterrey"
-
-Decidim::Budgets.workflows[:monterrey] = Decidim::Budgets::Workflows::Monterrey
