a0760b4235aec0999aa8f3e1fd3b68789e462c51
Ali G <ali@basicavisual.io>
2022-10-27 19:15:36 -0500

    new uploader with higher dimensions for Ine Module

---
+++ decidim-module-ine/app/uploaders/decidim/verifications/attachment_uploader.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Verifications
+    # This class deals with uploading attachments to a participatory space.
+    class AttachmentUploader < ApplicationUploader
+
+      def max_image_height_or_width
+        800000000000
+      end
+    end
+  end
+end
