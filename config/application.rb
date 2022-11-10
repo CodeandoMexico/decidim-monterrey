3e5275646621d63ce250232b637808675ff9be4e
Ali G <ali@basicavisual.io>
2022-11-09 19:00:03 -0600

    moved translations of managed_user_authorization_handler to the local instance as their pertain to a local authorization handler

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module DecidimMonterrey
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml").to_s]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
