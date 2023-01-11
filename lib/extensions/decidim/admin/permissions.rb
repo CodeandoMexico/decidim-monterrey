c18dbf4b08bc5a8d359822e1844b5a4d2a5b8940
Ali G <ali@basicavisual.io>
2023-01-11 17:33:20 -0600

    Fixes admin's permission to upload images on Quill editor

--- lib/extensions/decidim/admin/permissions.rb
+++ lib/extensions/decidim/admin/permissions.rb
@@ -3,27 +3,6 @@ module Extensions
     module Admin
       module Permissions
         def permissions
-          return permission_action if managed_user_action?
-
-          unless permission_action.scope == :admin
-            read_admin_dashboard_action?
-            return permission_action
-          end
-
-          unless user
-            disallow!
-            return permission_action
-          end
-
-          if user_manager?
-            begin
-              allow! if user_manager_permissions.allowed?
-            rescue ::Decidim::PermissionAction::PermissionNotSetError
-              nil
-            end
-          end
-
-          allow! if user_can_enter_space_area?(require_admin_terms_accepted: true)
 
           read_admin_dashboard_action?
           allow! if permission_action.subject == :global_moderation
@@ -37,33 +16,7 @@ module Extensions
             end
           end
 
-          if user.admin? && admin_terms_accepted?
-            allow! if read_admin_log_action?
-            allow! if read_metrics_action?
-            allow! if static_page_action?
-            allow! if organization_action?
-            allow! if user_action?
-
-            allow! if permission_action.subject == :category
-            allow! if permission_action.subject == :component
-            allow! if permission_action.subject == :admin_user
-            allow! if permission_action.subject == :attachment
-            allow! if permission_action.subject == :attachment_collection
-            allow! if permission_action.subject == :scope
-            allow! if permission_action.subject == :scope_type
-            allow! if permission_action.subject == :area
-            allow! if permission_action.subject == :area_type
-            allow! if permission_action.subject == :user_group
-            allow! if permission_action.subject == :officialization
-            allow! if permission_action.subject == :moderate_users
-            allow! if permission_action.subject == :authorization
-            allow! if permission_action.subject == :authorization_workflow
-            allow! if permission_action.subject == :static_page_topic
-            allow! if permission_action.subject == :help_sections
-            allow! if permission_action.subject == :share_token
-          end
-
-          permission_action
+          super
         end
 
         private
